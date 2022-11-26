2999afb34fceb596f26921f2f892d44410fcfe96
Will Taff <wrtaff@gmail.com>
2022-11-26 17:00:41 -0500

    Add files via upload

---
+++ wwosCite.js
@@ -0,0 +1,30 @@
+javascript: 
+
+/* A simple tool for cites in mediawiki for wwos*/
+
+
+/* alert('quote is marked'); */
+
+var mouseSelectedText = document.getSelection() ;
+
+/* alert(mouseSelectedText) ; */
+
+var today = new Date();
+
+var dd = String(today.getDate()).padStart(2, '0');
+
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+
+var yyyy = today.getFullYear();
+
+today = yyyy + '-' + mm + '-' + dd;
+
+/* alert(today); */
+
+var pageURL = window.location.href;
+
+var totalCiteString = "\"" + mouseSelectedText + "\"" + " \<ref\>" + pageURL + " retrieved " + today + "<\/ref\>";
+
+/*%20alert(totalCiteString);%20*/
+
+alert(totalCiteString);
